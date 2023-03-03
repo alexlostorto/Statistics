c63e9801021673eaad0bf14b8a5cbfe0eb1cacec
Alex Lo Storto <lostorto.business@gmail.com>
2023-03-03 21:09:46 +0000

    Add spearmanks rank formula

--- Formulas.py
+++ Formulas.py
@@ -14,8 +14,8 @@ def parseList(item, toNumber=True, log=True):
         if log:
             print(item)
         return item
-    elif isinstance(item, str):
-        item = item.replace('  ', ' ')
+    elif isinstance(item, str) or isinstance(item, int) or isinstance(item, float):
+        item = str(item).replace('  ', ' ')
         item = item.replace(', ', ',')
         if item.startswith('[') and item.endswith(']'):
             item = item[1:-1]
@@ -27,15 +27,17 @@ def parseList(item, toNumber=True, log=True):
         if log:
             print(item)
         return item
+    else:
+        raise ValueError("Input is not a list")
 
 
 def mean(dataset=[], roundto=3, log=True):
     dataset = parseList(dataset, log=False)
 
     if log:
-        print(f"Mean: {round(sum(dataset) / len(dataset), roundto)}")
+        print(f"Mean: {round(sum(dataset)/ len(dataset),roundto)}")
 
-    return round(sum(dataset) / len(dataset), roundto)
+    return round(sum(dataset)/len(dataset), roundto)
 
 
 def median(dataset=[], roundto=3, log=True):
@@ -46,8 +48,7 @@ def median(dataset=[], roundto=3, log=True):
     if type(middle) == int or middle.is_integer():
         median = dataset[int(middle-1)]
     else:
-        median = (dataset[math.ceil(middle)-1] +
-                  dataset[math.floor(middle)-1]) / 2
+        median = (dataset[math.ceil(middle)-1]+dataset[math.floor(middle)-1])/2
 
     if log:
         print(f"Median: {round(median, roundto)}")
@@ -56,9 +57,9 @@ def median(dataset=[], roundto=3, log=True):
 
 
 def standardDeviation(dataset=[], roundto=3, log=True):
-    dataset = parseList(dataset)
+    dataset = parseList(dataset, log=False)
 
-    n = len(dataset, log=False)
+    n = len(dataset)
     xSum = 0
     xSumSquared = 0
 
@@ -73,12 +74,52 @@ def standardDeviation(dataset=[], roundto=3, log=True):
 
 
 def skewness(dataset=[], roundto=3, log=True):
-    dataset = parseList(dataset)
+    dataset = parseList(dataset, log=False)
 
     if log:
-        print(f"Skewness: {round((3 * (mean(dataset, 5) - median(dataset, 5))) / standardDeviation(dataset, 5), roundto)}")
+        print(f"Skewness: {round((3*(mean(dataset, 5)-median(dataset, 5)))/standardDeviation(dataset, 5),roundto)}")
 
-    return round((3 * (mean(dataset, 5) - median(dataset, 5))) / standardDeviation(dataset, 5), roundto)
+    return round((3*(mean(dataset, 5)-median(dataset, 5)))/standardDeviation(dataset, 5), roundto)
+
+
+def spearmansRank(dataset1=[], dataset2=[], roundto=3, log=True):
+    dataset1, dataset2 = parseList(dataset1, log=False), parseList(dataset2, log=False)
+
+    def rank_simple(vector):
+        return sorted(range(len(vector)), key=vector.__getitem__)
+
+    def rankdata(a):
+        n = len(a)
+        ivec = rank_simple(a)
+        svec = [a[rank] for rank in ivec]
+        sumranks = 0
+        dupcount = 0
+        newarray = [0]*n
+        for i in range(n):
+            sumranks += i
+            dupcount += 1
+            if i == n-1 or svec[i] != svec[i+1]:
+                averank = sumranks / float(dupcount) + 1
+                for j in range(i-dupcount+1, i+1):
+                    newarray[ivec[j]] = averank
+                sumranks = 0
+                dupcount = 0
+        return newarray
+
+    if len(dataset1) != len(dataset2):
+        raise ValueError("Both lists must be the same length")
+
+    ranked1 = rankdata(dataset1)
+    ranked2 = rankdata(dataset2)
+
+    sumDifSquared = 0
+    for i in range(len(ranked1)):
+        sumDifSquared += abs(ranked1[i]-ranked2[i])**2
+
+    if log:
+        print(f"Spearmans rank: {round(1-((6*sumDifSquared)/(len(dataset1)*(len(dataset1)**2-1))), roundto)}")
+
+    return round(1-((6*sumDifSquared)/(len(dataset1)*(len(dataset1)**2-1))), roundto)
 
 
 __all__ = [
@@ -86,5 +127,6 @@ __all__ = [
     mean,
     median,
     standardDeviation,
-    skewness
+    skewness,
+    spearmansRank
 ]
